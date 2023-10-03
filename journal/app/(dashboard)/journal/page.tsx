32b513a7519f35c2b6bb9e5f6b10bb7dc9b5e5bf
PAPPY-JOE <fatoyejoseph@gmail.com>
2023-10-03 10:58:09 +0100

    modified page's height => h-full

--- journal/app/(dashboard)/journal/page.tsx
+++ journal/app/(dashboard)/journal/page.tsx
@@ -1,6 +1,7 @@
 import { getUserByClerkId } from '@/utils/auth';
 import { prisma } from '@/utils/db';
 import { EntryCard, NewEntryCard } from '@/components';
+import Link from 'next/link';
 
 const getEntries = async () => {
   const user = await getUserByClerkId()
@@ -20,11 +21,15 @@ const JournalPage = async () => {
   const entries = await getEntries()
 
   return (
-    <div className="p-10 bg-zinc-400/10"> 
+    <div className="p-10 bg-zinc-400/10 h-full"> 
       <h2 className="text-3xl mb-8">Journal</h2>
       <div className="grid grid-cols-3 gap-4">
         <NewEntryCard />
-        {entries.map((entry) => <EntryCard key={entry.id} entry={entry} />)}
+        {entries.map((entry) => 
+          <Link href={`/journal/${entry.id}`} key={entry.id} >  
+            <EntryCard entry={entry} />
+          </Link>
+          )}
       </div>    
     </div>
   )
