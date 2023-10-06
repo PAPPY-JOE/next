43966d0c6179a98a241c5ac81819670668408081
PAPPY-JOE <fatoyejoseph@gmail.com>
2023-10-06 11:49:01 +0100

    debugging

--- journal/app/new-user/loading.tsx
+++ journal/app/new-user/loading.tsx
@@ -1,1 +1,7 @@
 // whatever I want to show when the server-side user creation logic is going on is put here. For free!
+
+const LoadingPage = () => {
+  return <div>...loading</div>;
+};
+
+export default LoadingPage;
