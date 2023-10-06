4b0bc81324e262528b56c8541d2874faf71aeed8
PAPPY-JOE <fatoyejoseph@gmail.com>
2023-10-06 11:50:01 +0100

    implemented journal, edit-journal func

--- journal/app/(dashboard)/journal/[id]/page.tsx
+++ journal/app/(dashboard)/journal/[id]/page.tsx
@@ -1,7 +1,35 @@
-const EntryPage = ({params}) => {
+import { Editor } from '@/components'
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+
+const getEntry = async (id) => {
+    const user = await getUserByClerkId()
+    const entry = await prisma.journalEntry.findUnique({
+        where: { 
+            userId_id: { // this is now a complex variable since in the schema we did '@@unique([userId, id])' for JournalEntry 
+                userId: user.id,
+                id,
+            },
+        },            
+        include: {
+            analysis: true,
+        },
+    })
+
+    return entry
+}
+
+const EntryPage = async ({params}) => {
     // params.id because the page is name id, as in the folder
+
+    // I can pass serializable data from server component to client component, like in this case passing 'entry'
+    const entry = await getEntry(params.id)
+ 
     return (
-        <div>{params.id}</div>
+        <div className="h-full w-full">
+                <Editor entry={entry} />                        
+        </div>
     )
 }
 
--- journal/app/(dashboard)/journal/page.tsx
+++ journal/app/(dashboard)/journal/page.tsx
@@ -1,38 +1,41 @@
-import { getUserByClerkId } from '@/utils/auth';
-import { prisma } from '@/utils/db';
-import { EntryCard, NewEntryCard } from '@/components';
-import Link from 'next/link';
+import { getUserByClerkId } from "@/utils/auth";
+import { prisma } from "@/utils/db";
+import { EntryCard, NewEntryCard, Question } from "@/components";
+import Link from "next/link";
 
 const getEntries = async () => {
-  const user = await getUserByClerkId()
+  const user = await getUserByClerkId();
   const entries = await prisma.journalEntry.findMany({
     where: {
       userId: user.id,
     },
     orderBy: {
-      createdAt: 'desc'
+      createdAt: "desc",
     },
-  })
+  });
 
-  return entries
-}
+  return entries;
+};
 
 const JournalPage = async () => {
-  const entries = await getEntries()
+  const entries = await getEntries();
 
   return (
-    <div className="p-10 bg-zinc-400/10 h-full"> 
+    <div className="p-10 bg-zinc-400/10 h-full">
       <h2 className="text-3xl mb-8">Journal</h2>
+      <div className="my-8">
+        <Question />
+      </div>
       <div className="grid grid-cols-3 gap-4">
         <NewEntryCard />
-        {entries.map((entry) => 
-          <Link href={`/journal/${entry.id}`} key={entry.id} >  
+        {entries.map((entry) => (
+          <Link href={`/journal/${entry.id}`} key={entry.id}>
             <EntryCard entry={entry} />
           </Link>
-          )}
-      </div>    
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default JournalPage
+export default JournalPage;
