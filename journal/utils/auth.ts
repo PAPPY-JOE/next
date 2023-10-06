b4c947f78dc362b354a52c111a71c7d6b488a837
PAPPY-JOE <fatoyejoseph@gmail.com>
2023-10-06 11:46:19 +0100

    debugging

--- journal/utils/auth.ts
+++ journal/utils/auth.ts
@@ -1,16 +1,16 @@
-import { auth } from '@clerk/nextjs';
-import { prisma } from './db';
+import { auth } from "@clerk/nextjs";
+import { prisma } from "./db";
 
 export const getUserByClerkId = async ({ includes = {}, select = {} } = {}) => {
-    const { userId } = await auth()
+  const { userId } = await auth();
 
-    const user = await prisma.user.findUniqueOrThrow({
-        where: {
-            clerkId: userId as string,
-        },
-        // select,
-        // includes,
-    })
+  const user = await prisma.user.findUniqueOrThrow({
+    where: {
+      clerkId: userId as string,
+    },
+    // select,
+    // includes,
+  });
 
-    return user
-}
+  return user;
+};
